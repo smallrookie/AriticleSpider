404b7f0d18b58c365771c3a4f99b017c6ab9cf20
smallrookie <1543666575@qq.com>
2017-07-22 15:38:54 +0800

    利用selenium模拟新浪微博登录
    利用selenium模拟新浪微博登录

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -1,12 +1,24 @@
+import time
+
 from selenium import webdriver
 from scrapy.selector import Selector
 
 browser = webdriver.Chrome(executable_path="D:/Python/chromedriver_win32/chromedriver.exe")
 
 # 模拟知乎登录
-browser.get("https://www.zhihu.com/#signin")
+# browser.get("https://www.zhihu.com/#signin")
+#
+# browser.find_element_by_css_selector(".view-signin input[name='account']").send_keys("")
+# browser.find_element_by_css_selector(".view-signin input[name='password']").send_keys("")
+#
+# browser.find_element_by_css_selector(".view-signin button.sign-button").click()
 
-browser.find_element_by_css_selector(".view-signin input[name='account']").send_keys("")
-browser.find_element_by_css_selector(".view-signin input[name='password']").send_keys("")
+# 模拟新浪微博登录
+browser.get("https://www.weibo.com/")
 
-browser.find_element_by_css_selector(".view-signin button.sign-button").click()
+time.sleep(15)
+
+browser.find_element_by_css_selector("#loginname").send_keys("")
+browser.find_element_by_css_selector(".info_list.password input[name='password']").send_keys("")
+
+browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()

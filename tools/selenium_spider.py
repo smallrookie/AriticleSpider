f5ed3049cf056e0abe50a6de54b3707975fc81b2
smallrookie <1543666575@qq.com>
2017-07-22 15:53:50 +0800

    利用selenium模拟鼠标下滑操作
    利用selenium模拟鼠标下滑操作

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -14,11 +14,21 @@ browser = webdriver.Chrome(executable_path="D:/Python/chromedriver_win32/chromed
 # browser.find_element_by_css_selector(".view-signin button.sign-button").click()
 
 # 模拟新浪微博登录
-browser.get("https://www.weibo.com/")
+# browser.get("https://www.weibo.com/")
+#
+# time.sleep(15)
+#
+# browser.find_element_by_css_selector("#loginname").send_keys("")
+# browser.find_element_by_css_selector(".info_list.password input[name='password']").send_keys("")
+#
+# browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
 
-time.sleep(15)
+# 模拟鼠标下滑操作
+browser.get("https://www.oschina.net/blog")
 
-browser.find_element_by_css_selector("#loginname").send_keys("")
-browser.find_element_by_css_selector(".info_list.password input[name='password']").send_keys("")
+time.sleep(5)
 
-browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
+for i in range(3):
+    browser.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight; return lenOfPage")
+    time.sleep(3)

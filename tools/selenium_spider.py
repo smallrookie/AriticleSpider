1d8673f897e24f95ce5364806876baa3408a9abc
smallrookie <1543666575@qq.com>
2017-07-22 19:46:49 +0800

    phantomjs获取动态页面
    phantomjs获取动态页面

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -34,8 +34,14 @@ from scrapy.selector import Selector
 #     time.sleep(3)
 
 # 设置chromedriver不加载图片
-chrome_opt = webdriver.ChromeOptions()
-prefs = {"profile.managed_default_content_settings.images":2}
-chrome_opt.add_experimental_option("prefs", prefs)
-browser = webdriver.Chrome(executable_path="D:/Python/chromedriver_win32/chromedriver.exe", chrome_options=chrome_opt)
-browser.get("https://www.taobao.com")
+# chrome_opt = webdriver.ChromeOptions()
+# prefs = {"profile.managed_default_content_settings.images":2}
+# chrome_opt.add_experimental_option("prefs", prefs)
+# browser = webdriver.Chrome(executable_path="D:/Python/chromedriver_win32/chromedriver.exe", chrome_options=chrome_opt)
+# browser.get("https://www.taobao.com")
+
+# phantomjs获取动态页面
+browser = webdriver.PhantomJS(executable_path="D:/Python/phantomjs-2.1.1-windows/bin/phantomjs.exe")
+browser.get("https://detail.tmall.com/item.htm?spm=a1z10.1-c-s.w12075169-15146113259.31.3910d3c510iTDI&id=540677596153&scene=taobao_shop&sku_properties=10004:1162302684;5919063:6536025")
+print(browser.page_source)
+browser.quit()
